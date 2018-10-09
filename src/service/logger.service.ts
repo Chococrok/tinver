cce5fb0029780cbe84a5a7ce823c41fd1786bccc
chococrok <moi_viking@hotmail.com>
2018-10-09 18:46:47 +0200

    first commit

---
+++ src/service/logger.service.ts
@@ -0,0 +1,29 @@
+import * as winston from "winston";
+
+export abstract class LoggerService {
+  private static WINSTON_LOGGER: winston.Logger;
+
+  private static configureLogger() {
+    LoggerService.WINSTON_LOGGER = winston.createLogger({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.printf(info => {
+          return `${new Date(info.timestamp).toLocaleString()} ${info.level}: ${info.message}`;
+        }),
+      ),
+      transports: [
+        new winston.transports.Console(),
+      ],
+    });
+  }
+
+  public static getLogger(): winston.Logger {
+    if (!LoggerService.WINSTON_LOGGER) {
+      this.configureLogger();
+    }
+
+    return LoggerService.WINSTON_LOGGER;
+  }
+
+}
